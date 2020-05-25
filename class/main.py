cb7e801de6e2e2e71f71839a6ffb3eee7a2b9479
welandx <59045899+welandx@users.noreply.github.com>
2020-05-25 17:33:15 +0800

    Add files via upload
    更新搜索，综合搜索未完成

--- class/main.py
+++ class/main.py
@@ -5,23 +5,48 @@ c.seek(78,os.SEEK_CUR)  # 移动指针
 i=1
 j=1
 from tkinter import *
+import re
 
-
-class lesson :
+class Lesson:
     list1 = []  # 存放所有课程信息
     # 测试函数
-    def doweekseach(self,var):
+    def judge(self,var):
+        if re.match('星期\d',var):
+            x=re.search('\d',var)
+            self.doweekseek(int(x.group()))
+        elif re.match('第\d节',var):
+            x = re.search('\d', var)
+            self.secnumseek(int(x.group()))
+        elif re.match('[^x00-xff]',var):
+            x=re.match('[^x00-xff]+',var)
+            print(x.group())
+            self.nameseek(x.group())
+    def doweekseek(self,var):
         text = Text(top, width=17, height=11)  # 创建文本控件
         text.place(x=1,y=1)  # 在屏幕上放置文本控件
         for x in self.list1:
             if x['doweek']==var:
                 src='星期%d 第%s节 %s' %(x['doweek'],x['secnum'],x['name'])
                 text.insert(INSERT,src)  # 在控件上放置文本
-lesson1=lesson()
+    def secnumseek(self,var):
+        text = Text(top, width=17, height=11)  # 创建文本控件
+        text.place(x=1, y=1)  # 在屏幕上放置文本控件
+        for x in self.list1:
+            if x['secnum'] == var:
+                src = '星期%d 第%s节 %s' % (x['doweek'], x['secnum'], x['name'])
+                text.insert(INSERT, src)  # 在控件上放置文本
+    def nameseek(self,var):
+        text = Text(top, width=17, height=11)  # 创建文本控件
+        text.place(x=1, y=1)  # 在屏幕上放置文本控件
+        for x in self.list1:
+            if x['name'] == var:
+                src = '星期%d 第%s节 %s' % (x['doweek'], x['secnum'], x['name'])
+                text.insert(INSERT, src)  # 在控件上放置文本
+lesson1=Lesson()
 
 
 top = Tk()  # 创建一个窗体
-top.geometry("1100x400+200+50") # 改变窗体的大小
+top.geometry("1100x400+200+50")  # 改变窗体的大小
 
 
 # 读取与显示
@@ -70,7 +95,7 @@ entry.pack()
 # 搜索按钮
 def insert_point():
     var = entry.get()  # 获取输入内容
-    lesson1.doweekseach(int(var))  # 测试
+    lesson1.judge(var)  # 测试
 b1 = Button(top,text="搜索",width=15,height=2,command=insert_point)  # 按钮，绑定事件insert_input
 b1.pack()
 
